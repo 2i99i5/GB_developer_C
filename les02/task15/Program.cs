28ee68e47e42450b9781728101793f46eb180780
zakirovra <zakirovra@gmail.com>
2022-09-24 22:11:23 +0400

    add some changes

--- les02/task15/Program.cs
+++ les02/task15/Program.cs
@@ -6,11 +6,15 @@
 7 -> да
 1 -> нет
 */
+string WeekEnd (int dayNum)
+{
+    if (dayNum >= 1 && dayNum <= 5) return "нет";
+    else if (dayNum >= 6 && dayNum <= 7) return "да";
+    return "Введено что-то отличное от цифры из диапазона от 1 до 7";
+}
+
 
 Console.WriteLine("Введите цифру, обозначающую день недели");
 int dayNum = int.Parse(Console.ReadLine()??"");
 
-Console.Write("Это выходной день? ");
-if (dayNum >= 1 && dayNum <= 5) Console.WriteLine("нет");
-else if (dayNum >= 6 && dayNum <= 7) Console.WriteLine("да");
-else Console.WriteLine("Введено что-то отличное от цифры из диапазона от 1 до 7");
+Console.Write($"Это выходной день? - {WeekEnd (dayNum)}");
