9b8ba2e553e8a178d58ce3f50d243046e9fc1b53
zakirovra <zakirovra@gmail.com>
2022-10-03 11:22:07 +0400

    add hw5

---
+++ les05/task34/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+//метод инициализации массива
+int [] InitArray(int demension)
+{
+    int[] arr = new int[demension];
+    Random rnd = new Random();
+    for (int i = 0; i < demension; i++)
+    {
+        arr[i] = rnd.Next(100,1000);
+    }
+    return arr;
+}
+
+//метод, который печатает массив на консоль
+void PrintArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine($"]");
+}
+
+//метод, который считает четные числа в массиве
+int GetCountOfEvenElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0) count += 1;
+    }
+    return count;
+}
+
+int [] arr = InitArray(4);
+PrintArray(arr);
+
+int positiveCount = GetCountOfEvenElements(arr);
+
+Console.WriteLine($"Количество чётных элементов = {positiveCount}");
---
+++ les05/task38/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным 
+и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+//метод инициализации массива
+float [] InitArray(int demension)
+{
+    float[] arr = new float[demension];
+    Random rnd = new Random();
+    for (int i = 0; i < demension; i++)
+    {
+        arr[i] = rnd.Next(0,100);
+    }
+    return arr;
+}
+
+//метод, который печатает массив на консоль
+void PrintArray(float[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine($"]");
+}
+
+//метод, который находит разность максимального и минимального элементы массива
+float GetDiffOfMaxMinElements(float[] array)
+{
+    float min = array[0];
+    float max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]>max) max = array[i];
+        if (array[i]<min) min = array[i];
+    }
+    return max-min;
+}
+
+float [] arr = InitArray(5);
+PrintArray(arr);
+float difMaxMin = GetDiffOfMaxMinElements(arr);
+Console.WriteLine($"Разность максимального и минимального = {difMaxMin}");
+Console.WriteLine();
+
+float[] array1 = new float[] { 3, 7, 22, 2, 78 };
+PrintArray(array1);
+Console.WriteLine($"Разность максимального и минимального = {GetDiffOfMaxMinElements(array1)}");
