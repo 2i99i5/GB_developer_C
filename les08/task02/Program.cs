705ea00c348eb393573fd318aed56abf26c50cde
zakirovra <zakirovra@gmail.com>
2022-10-13 15:27:49 +0400

    add les08

---
+++ les08/task02/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке 
+и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int MinSumRow(int[,] matrix)
+{
+    int numRow = 0;
+    int sumMin = 2147483647;
+    int sum;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (sumMin > sum)
+        {
+            sumMin = sum;
+            numRow = i;
+        }
+    }
+    return numRow + 1;  // переход на "человеческий" порядок строк
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
+
+Console.WriteLine($"Наименьшая сумма элементов в {MinSumRow(matrix)} строке");
---
+++ les08/task03/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiplicationOfMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+    for (int i = 0; i < matrix1.GetLength(0); ++i)
+        for (int j = 0; j < matrix2.GetLength(0); ++j)
+            for (int k = 0; k < matrix2.GetLength(1); ++k)
+                result[i, k] += matrix1[i, j] * matrix2[j, k];
+    return result;
+}
+
+int m = GetNumber("Введите число m1");
+int n = GetNumber("Введите число n1");
+int[,] matrix1 = InitMatrix(m, n);
+m = GetNumber("Введите число m2");
+n = GetNumber("Введите число n2");
+int[,] matrix2 = InitMatrix(m, n);
+
+Console.WriteLine("Матрица1:");
+PrintMatrix(matrix1);
+Console.WriteLine("");
+Console.WriteLine("Матрица2:");
+PrintMatrix(matrix2);
+
+if (matrix1.GetLength(1) != matrix2.GetLength(0))
+{
+    Console.WriteLine("Матрицы нельзя перемножить");
+}
+else
+{
+    int[,] result = MultiplicationOfMatrix(matrix1, matrix2);
+    Console.WriteLine("Произведение матриц");
+    PrintMatrix(result);
+}
+Console.WriteLine("");
+
+int[,] ar1 = { { 2, 4 }, { 3, 2 } };
+int[,] ar2 = { { 3, 4 }, { 3, 3 } };
+Console.WriteLine("Матрица1:");
+PrintMatrix(ar1);
+Console.WriteLine("Матрица2:");
+PrintMatrix(ar2);
+
+Console.WriteLine("Произведение матриц");
+PrintMatrix(MultiplicationOfMatrix(ar1, ar2));
