d350c7dde8c4be87952890a2f261407069cc4d2b
zakirovra <zakirovra@gmail.com>
2022-10-17 17:09:49 +0400

    add les09

---
+++ les09/task02/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int RecursionNum(int m, int n, int sum = 0)
+{
+    if (m > n) // проверка, что первое число меньше второго
+    {
+        int tmp;
+        tmp = n;
+        n = m;
+        m = tmp;
+    }
+    if (m == n)
+    {
+        return sum + m;
+    }
+    sum += m;
+    return RecursionNum(m + 1, n, sum);
+}
+
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+
+Console.Write($"{RecursionNum(m, n)}");
---
+++ les09/task03/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int Akkerman(int n, int m)
+{
+  if (n == 0) return m + 1;
+  else if ((n != 0) && (m == 0)) return Akkerman(n - 1, 1);
+    else return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+
+Console.Write($"{Akkerman(m, n)}");
